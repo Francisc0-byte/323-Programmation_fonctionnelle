54fb0391f797156ff61df5a27202d7738f44a30a
Francisc0-byte <pc96ihd@eduvaud.ch>
2025-09-11 11:59:26 +0200

    feat
    debut de l'exercice mib-map

---
+++ francisco/mib-map/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Common;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+class Program
+{
+    
+    static void Main()
+    {
+        List<Product> products = new List<Product>
+        {
+            new Product { Location = 1, Provider = "Bornand", ProductName = "Pommes", Quantity = 20,Unit = "kg", Price = 5.50f },
+            new Product { Location = 1, Provider = "Bornand", ProductName = "Poires", Quantity = 16,Unit = "kg", Price = 5.50f },
+            new Product { Location = 1, Provider = "Bornand", ProductName = "Pastèques", Quantity = 14,Unit = "pièce", Price = 5.50f },
+            new Product { Location = 1, Provider = "Bornand", ProductName = "Melons", Quantity = 5,Unit = "kg", Price = 5.50f },
+            new Product { Location = 2, Provider = "Dumont", ProductName = "Noix", Quantity = 20,Unit = "sac", Price = 5.50f },
+            new Product { Location = 2, Provider = "Dumont", ProductName = "Raisin", Quantity = 6,Unit = "kg", Price = 5.50f },
+            new Product { Location = 2, Provider = "Dumont", ProductName = "Pruneaux", Quantity = 13,Unit = "kg", Price = 5.50f },
+            new Product { Location = 2, Provider = "Dumont", ProductName = "Myrtilles", Quantity = 12,Unit = "kg", Price = 5.50f },
+
+            //...
+
+            
+        };
+        //var nomAnonymes = products.Select(p => p.Provider.Length > 4 ? p.Provider.Substring(0, 3) + "..." : p.Provider);
+
+        var ap = products.Select(product => new AnonProduct() { 
+            ProviderAnon = product.Provider.Length > 4 ? product.Provider.Substring(0, 3) + "..." + product.Provider[^1] : product.Provider, 
+            Name = "pasteque", 
+            CA = product.Quantity * product.Price 
+        });
+
+        Console.WriteLine(String.Join('\n',ap));
+    }
+
+    class AnonProduct
+    {
+        public string ProviderAnon { get; set; }
+        public string Name { get; set; }
+        public float CA { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ProviderAnon},{Name},{CA}";
+        }
+    }
+
+    class Product
+    {
+        public int Location { get; set; }
+
+        public string Provider { get; set; }
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+        public float Price { get; set; }
+    }
+
+}
+
--- francisco/words/word_b/Program.cs
+++ francisco/words/word_b/Program.cs
@@ -10,21 +10,28 @@ class Program
 
         Action a = () =>
         {
+            //partie a
             string[] words = { "whatThe!!!", "bonjour", "hello", "monde", "vert", "rouge", "bleu", "jaune", "My kingdom for a horse !", "Ooops I did it again" };
 
             Console.WriteLine($"{string.Join(", ", words.Skip(1).SkipLast(2))}");
 
+            //partie b
             string[] wordsSpecial = { "+++++", "<<<<<", ">>>>>", "bonjour", "hello", "@@@@", "vert", "rouge", "bleu", "jaune", "#####", "%%%%%%%" };
 
             var clean = wordsSpecial.Where(word => word.All(letter => char.IsLetterOrDigit(letter)));
             Console.WriteLine(string.Join(" ", clean));
 
+            //partie c
             string[] wordsElite = { "i am the winner", "hello", "monde", "vert", "rouge", "bleu", "i am the looser" };
 
             Console.WriteLine($"Winner: {wordsElite.First()}\nLosser: {wordsElite.Last()}");
 
         };
             a();
+        Action epsilon = () => { 
+        
+
+        };
 
     }
 
