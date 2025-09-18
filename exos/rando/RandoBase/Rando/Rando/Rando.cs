958c59c36ca44a04b00125d69523d1a2ad1ada71
Francisc0-byte <pc96ihd@eduvaud.ch>
2025-09-18 12:12:53 +0200

    feat
    ex min reduce

--- exos/rando/RandoBase/Rando/Rando/Rando.cs
+++ exos/rando/RandoBase/Rando/Rando/Rando.cs
@@ -1,10 +1,55 @@
+﻿using Gpx;
+
 namespace Rando
 {
     public partial class Rando : Form
     {
+        string gpxFile = @"..\..\..\..\..\..\gpx\Ballade_châtaignère.gpx";
+        List<TrackPoint> trackPoints = new();
+
         public Rando()
         {
             InitializeComponent();
+            if (!File.Exists(gpxFile))
+            {
+                MessageBox.Show($"Fichier {gpxFile} pas trouvé");
+            }
+
+            StreamReader stream = new StreamReader(gpxFile);
+
+            using (GpxReader reader = new GpxReader(stream.BaseStream))
+            {
+                while (reader.Read())
+                {
+                    switch (reader.ObjectType)
+                    {
+                        case GpxObjectType.Track:
+                        var gpxPoints = reader.Track.ToGpxPoints();
+                            /*
+                                //fonctionne mais uniquement si on a qu'une track puisque elle 
+                                //ecrase à chaque fois
+                                points = gpxPoints.Select(gpxPoint => new TrackPoint()
+                                {
+                                    Elevation = gpxPoint.Elevation,
+                                    Latitute = gpxPoint.Latitude,
+                                    Longitute = gpxPoint.Longitude
+
+                                }).ToList();
+                            */
+                            var converted = gpxPoints.Select(gpxPoint => new TrackPoint()
+                            {
+                                Elevation = gpxPoint.Elevation,
+                                Latitude = gpxPoint.Latitude,
+                                Longitude = gpxPoint.Longitude
+
+                            }).ToList();
+                            //AddRange permet d'ajouter plusieurs point
+                            trackPoints.AddRange(converted.ToList());
+                            break;
+                    }
+                }
+            }
+
         }
 
         private void Rando_Form_Paint(object sender, PaintEventArgs e)
@@ -12,8 +57,23 @@ namespace Rando
             Pen myPen = new Pen(Color.Red);
             myPen.Width = 2;
 
-            Point[] points = new Point[4] { new Point(30,50), new Point(50,10), new Point(80,50), new Point(111,400) };
+            //Point[] points = new Point[4] { new Point(30,50), new Point(50,10), new Point(80,50), new Point(111,400) };
+
+            var minLat = trackPoints.Min(tp => tp.Latitude);
+            var maxLat = trackPoints.Max(tp => tp.Latitude);
+
+            var minLon = trackPoints.Min(tp => tp.Longitude);
+            var maxLon = trackPoints.Max(tp => tp.Longitude);
+            // De trackpoint vers POINT
+            Point[] points = trackPoints.Select(trackPoint => new Point()
+            {
+                X = Convert.ToInt32(trackPoint.Longitude),
+                Y = Convert.ToInt32(trackPoint.Latitude)
+            }).ToArray();
+
             this.CreateGraphics().DrawLines(myPen, points);
+
+            
         }
     }
 }
---
+++ francisco/mib-reduce/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Common;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+class Program
+{
+    
+    static void Main()
+    {
+        List<Product> products = new List<Product>
+        {
+            new Product { Location = 1, Provider = "Bornand", ProductName = "Pommes", Quantity = 20,Unit = "kg", Price = 5.50f },
+            new Product { Location = 1, Provider = "Bornand", ProductName = "Poires", Quantity = 16,Unit = "kg", Price = 5.50f },
+            new Product { Location = 1, Provider = "Bornand", ProductName = "Pastèques", Quantity = 14,Unit = "pièce", Price = 5.50f },
+            new Product { Location = 1, Provider = "Bornand", ProductName = "Melons", Quantity = 5,Unit = "kg", Price = 5.50f },
+            new Product { Location = 2, Provider = "Dumont", ProductName = "Noix", Quantity = 20,Unit = "sac", Price = 5.50f },
+            new Product { Location = 2, Provider = "Dumont", ProductName = "Raisin", Quantity = 6,Unit = "kg", Price = 5.50f },
+            new Product { Location = 2, Provider = "Dumont", ProductName = "Pruneaux", Quantity = 13,Unit = "kg", Price = 5.50f },
+            new Product { Location = 2, Provider = "Dumont", ProductName = "Myrtilles", Quantity = 12,Unit = "kg", Price = 5.50f },
+            new Product { Location = 10, Provider = "Cherix", ProductName = "Myrtilles", Quantity = 9, Unit = "kg", Price = 4.20f },
+            new Product { Location = 10, Provider = "Cherix", ProductName = "Groseilles", Quantity = 10, Unit = "kg", Price = 4.40f },
+            new Product { Location = 5, Provider = "Blanc", ProductName = "Pruneaux", Quantity = 5, Unit = "kg", Price = 9.00f },
+            new Product { Location = 5, Provider = "Blanc", ProductName = "Myrtilles", Quantity = 18, Unit = "kg", Price = 5.60f },
+            new Product { Location = 5, Provider = "Blanc", ProductName = "Groseilles", Quantity = 10, Unit = "kg", Price = 2.10f },
+            new Product { Location = 5, Provider = "Blanc", ProductName = "Pêches", Quantity = 20, Unit = "kg", Price = 6.40f }
+        };
+        //0. La quantité de groseilles disponibles sur le marché
+        string choice = "Groseilles";
+        var quantiteDispo = products.Where(produit => produit.ProductName ==choice).Sum(produit => produit.Quantity);
+        Console.WriteLine($"Il y a {quantiteDispo} {choice} au total dans le marché");
+
+        //1.Le chiffre d’affaire possible **total * *pour chaque marchand(tout produit confondu)
+        var chiffreAff = products.GroupBy(p => p.Provider).Select(groupe => new
+        {
+            //on cree une nouvelle liste anonyme
+            Provider = groupe.Key,
+            chiffreAff = groupe.Sum(p => p.Price * p.Quantity),
+        });
+
+        foreach (var item in chiffreAff){
+            Console.WriteLine($"Le chiffre d'affaire total pour {item.Provider} et de {item.chiffreAff} fr" );
+        }
+        var chiffre = chiffreAff.Select(p => p.chiffreAff);
+        var chiffreMoyMax = chiffre.Max();
+        var chiffreMoyMin = chiffre.Min();
+        var chiffreMoyAvg = chiffre.Average();
+        //2. Le plus grand, le plus petit et la moyenne de ces chiffres d’affaire
+
+        Console.WriteLine($"le plus grand chiffre d'affaire est {chiffreMoyMax}");
+        Console.WriteLine($"le plus petit chiffre d'affaire est {chiffreMoyMin}");
+        Console.WriteLine($"La moyenne de chiffre d'affaire est {chiffreMoyAvg}");
+
+    }
+
+    class AnonProduct
+    {
+        public string ProviderAnon { get; set; }
+        public string Name { get; set; }
+        public float CA { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ProviderAnon},{Name},{CA}";
+        }
+    }
+
+    class Product
+    {
+        public int Location { get; set; }
+
+        public string Provider { get; set; }
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+        public float Price { get; set; }
+    }
+
+}
+
