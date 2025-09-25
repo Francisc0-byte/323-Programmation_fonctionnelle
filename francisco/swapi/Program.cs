4b1e8d9eb5295560dbaaf6985f13b229e9a856c8
Francisc0-byte <pc96ihd@eduvaud.ch>
2025-09-25 11:55:24 +0200

    debut de l'éxercice

---
+++ francisco/swapi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace Swapi
+{
+
+    public partial class Swapi
+    {
+        private HttpClient client = new HttpClient();
+
+        string HttpGet(HttpClient client, string query)
+        {
+            var json = HttpGetAsync(client, query).ConfigureAwait(false).GetAwaiter().GetResult();
+            return json;
+        }
+        async Task<string> HttpGetAsync(HttpClient client, string query)
+        {
+            var response = await client.GetAsync(query.Contains("https") ? query : "https://swapi.dev/api/" + query);
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+
+            return json;
+        }
+    }
+}
